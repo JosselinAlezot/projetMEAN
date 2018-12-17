f575bc3e4e03f6897e3aded7bf4738f14056a9dc
jalezot <jalezot@n11.fdsetu.infra.umontpellier.fr>
2018-12-17 14:41:04 +0100

    remise a zero

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthComponentComponent } from './auth-component/auth-component.component';
-import { ProductDisplayComponent } from './product-display/product-display.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AuthComponentComponent,
-    ProductDisplayComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
